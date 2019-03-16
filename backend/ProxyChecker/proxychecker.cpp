e3661ed3fd1a56707fcad0686b5472d1f330534b
Carlos Enrique Pérez Sánchez <thecrowporation@gmail.com>
2019-03-16 08:19:11 -0400

    Fix a bug that setup 2 threads for each network request. Now are created as much threads as requests, never more

--- backend/ProxyChecker/proxychecker.cpp
+++ backend/ProxyChecker/proxychecker.cpp
@@ -28,19 +28,10 @@ void ProxyChecker::start(const QNetworkRequest &request)
     QTimer t;
     t.setSingleShot(true);
 
-    QEventLoop loop;
-    connect(&t, &QTimer::timeout, &loop, &QEventLoop::quit);
-    connect(reply, &QNetworkReply::finished, &loop, &QEventLoop::quit);
+    connect(&t, &QTimer::timeout, reply, &QNetworkReply::abort);
+    connect(reply, &QNetworkReply::finished, &t, &QTimer::stop);
 
     t.start(timeout);
-    loop.exec();
-
-    if (t.isActive()) {
-        t.stop();
-    } else {
-        disconnect(reply, &QNetworkReply::finished, &loop, &QEventLoop::quit);
-        reply->abort();
-    }
 }
 
 void ProxyChecker::stop()
