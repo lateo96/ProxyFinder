460788140e7e2e9aff232330e6d36aad588135a7
Carlos Enrique Pérez Sánchez <thecrowporation@gmail.com>
2019-03-17 03:40:48 -0400

    Implement the model. Currently not functional due to bugs

--- backend/models/ReportModel/reportmodel.cpp
+++ backend/models/ReportModel/reportmodel.cpp
@@ -1,6 +1,80 @@
 #include "reportmodel.h"
+#include <QDebug>
 
-ReportModel::ReportModel(QObject *parent) : QObject(parent)
+ReportModel::ReportModel(QList<ProxyInfo *> dataReport, QObject *parent) : QAbstractListModel(parent)
 {
+    report = dataReport;
+}
 
+int ReportModel::rowCount(const QModelIndex &parent) const
+{
+    (void)parent;
+    //qDebug() << report.count();
+    return report.count();
+}
+
+QVariant ReportModel::data(const QModelIndex &index, int role) const
+{
+    switch (role) {
+    case HostNameRole:
+        return report[index.row()]->getHostName();
+    case HttpStatusCodeRole:
+        return report[index.row()]->getHttpStatusCode();
+    case HttpReasonPhraseRole:
+        return report[index.row()]->getHttpReasonPhrase();
+    default:
+        return QVariant();
+    }
+}
+
+QHash<int, QByteArray> ReportModel::roleNames() const
+{
+    QHash<int, QByteArray> roles;
+    roles[HostNameRole] = "hostName";
+    roles[HttpStatusCodeRole] = "httpStatusCode";
+    roles[HttpReasonPhraseRole] = "httpReasonPhrase";
+    return roles;
+}
+
+bool ReportModel::insertRows(int row, int count, const QModelIndex &parent)
+{
+    qDebug() << "Insert rows";
+    if (row <= 0 || row > report.count()) {
+        return false;
+    }
+    beginInsertRows(parent, row, row + count);
+    endInsertRows();
+    return true;
+}
+
+bool ReportModel::removeRows(int row, int count, const QModelIndex &parent)
+{
+    qDebug() << "Remove rows";
+    if (row < 0 || row >= report.count()) {
+        return false;
+    }
+    beginRemoveRows(parent, row, row + count);
+    for (int i = row; i <= row + count; ++i) {
+        report.removeAt(i);
+    }
+    qDebug() << report.count();
+    endRemoveRows();
+    return true;
+}
+
+QList<ProxyInfo *> ReportModel::getReport() const
+{
+    return report;
+}
+
+void ReportModel::append(ProxyInfo *info)
+{
+    report.append(info);
+    insertRow(rowCount());
+}
+
+void ReportModel::clear()
+{
+    removeRows(0, report.count());
+    //report.clear();
 }
--- backend/models/ReportModel/reportmodel.h
+++ backend/models/ReportModel/reportmodel.h
@@ -1,17 +1,37 @@
 #ifndef REPORTMODEL_H
 #define REPORTMODEL_H
 
-#include <QObject>
+#include <QAbstractListModel>
+#include "../../ProxyInfo/proxyinfo.h"
 
-class ReportModel : public QObject
+class ReportModel : public QAbstractListModel
 {
     Q_OBJECT
 public:
-    explicit ReportModel(QObject *parent = nullptr);
+    explicit ReportModel(QList<ProxyInfo*> dataReport = QList<ProxyInfo*>(), QObject *parent = nullptr);
+
+    enum Roles { HostNameRole = Qt::UserRole + 1, HttpStatusCodeRole, HttpReasonPhraseRole };
+    Q_ENUM(Roles)
+
+    // Pure virtual functions
+    int rowCount(const QModelIndex &parent = QModelIndex()) const;
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const;
+    QHash<int, QByteArray> roleNames() const;
+
+    // Others virtual functions
+    bool insertRows(int row, int count, const QModelIndex &parent = QModelIndex());
+    bool removeRows(int row, int count, const QModelIndex &parent = QModelIndex());
+
+    QList<ProxyInfo *> getReport() const;
+    void append(ProxyInfo *info);
 
 signals:
 
 public slots:
+    void clear();
+
+private:
+    QList<ProxyInfo*> report;
 };
 
 #endif // REPORTMODEL_H
